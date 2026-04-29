263c56228110b53453ba0d1e7afa0080d3afb0dc
carly-dempsey04 <cd39227@my.utexas.edu>
2026-04-29 15:51:34 -0500

    Initial commit

---
+++ R/collaborativedatascience-package.R
@@ -0,0 +1,27 @@
+#' collaborativedatascience-package
+#'
+#' A package for analyzing benthic cover data from coral reef ecosystems,
+#' including data wrangling, visualization, ordination, and modeling tools
+#' developed for a collaborative data science project.
+#'
+#' @details
+#' The collaborativedatasciencepackage provides functions and datasets
+#' to support ecological analysis of coral reef benthic communities.
+#' Key features include:
+#'
+#' \itemize{
+#'   \item Data preparation and wrangling of quadrat-level observations
+#'   \item Functional group aggregation of benthic categories
+#'   \item Multivariate ordination (PCoA)
+#'   \item Visualization utilities for benthic community data
+#'   \item Modeling tools for coral–macroalgae relationships
+#' }
+#'
+#' @section Package options:
+#' This package does not currently define any global options.
+#'
+#' @author
+#' Carly R. Dempsey
+#'
+#' @keywords internal
+"_PACKAGE"
